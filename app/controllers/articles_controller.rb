74a347c2ab18e825f1b1a6889373a426ad767454
HinoShun <shumpei.hino88@icloud.com>
2023-03-11 14:42:19 +0900

    記事投稿機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,18 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, :image).merge(user_id: current_user.id)
+  end
+
 end
