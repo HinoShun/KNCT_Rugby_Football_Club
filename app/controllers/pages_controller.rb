a91fc2c507b5efa95f92a3b5fe43313eb5ff3a22
HinoShun <shumpei.hino88@icloud.com>
2023-03-15 16:15:47 +0900

    一部ページ認証導入

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  before_action :basic, only: [:news_letter]
+
+
   def articles
     @articles = Article.order("id DESC")
   end
@@ -7,6 +10,15 @@ class PagesController < ApplicationController
   end
 
   def member
+  end
+
+  def news_letter
+  end
+
+  def basic
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASS']
+    end
   end
 
 end
